399efec02a29f0c2cb9ecd4f3c2f32d3afa2fd10
Nicholas McKibben <nicholas.bgp@gmail.com>
2021-10-04 21:17:13 -0700

    VER: bump version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ include_dirs = [
 
 setup(
     name='scikit-glpk',
-    version='0.4.2',
+    version='0.4.3',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     url='https://github.com/mckib2/scikit-glpk',
